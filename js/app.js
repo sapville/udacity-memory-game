45a20cf053d12aa4ae430fa7d215e874e565ec0c
Evgenii Fedoseev <abap@sapville.com>
2017-10-11 18:09:05 +0300

    feat: add entry form
    -add a form for the number of cells input

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 function main() { //eslint-disable-line no-unused-vars
-  console.log('invoked');
+  setLayout();
+}
+
+function setLayout() {
+
+  if (Boolean(true)) {
+    if (!$('#app').hasClass('hidden')) { //hide app if visible
+      $('#app').toggleClass('hidden');
+    }
+    if ($('#entry-form').hasClass('hidden')) { //show entry-form if hidden
+      $('#entry-form').toggleClass('hidden');
+    }
+    $('#pass-size').val($('#size').val());
+  } else {
+    if ($('#app').hasClass('hidden')) { //show app if hidden
+      $('#app').toggleClass('hidden');
+    }
+    if (!$('#entry-form').hasClass('hidden')) { //hide entry-form if visible
+      $('#entry-form').toggleClass('hidden');
+    }
+  }
 }
