0e15dbf77d09d718774e7dc216108132891c1277
Evgenii Fedoseev <abap@sapville.com>
2017-10-12 23:41:35 +0300

    feat: game reset via submit

--- js/app.js
+++ js/app.js
@@ -4,19 +4,23 @@ function main() { //eslint-disable-line no-unused-vars
 
 function setLayout() {
 
-  if (Boolean(true)) {
-    if (!$('#app').hasClass('hidden')) { //hide app if visible
-      $('#app').toggleClass('hidden');
-    }
-    if ($('#entry-form').hasClass('hidden')) { //show entry-form if hidden
-      $('#entry-form').toggleClass('hidden');
-    }
-    $('#pass-size').val($('#size').val());
-  } else {
+  const dimension = $.url('?size');
+
+
+  if ($.url('?do-reset') === 'X') { //launch the app
+    $('#save-dimension').val(dimension);
     if ($('#app').hasClass('hidden')) { //show app if hidden
       $('#app').toggleClass('hidden');
     }
     if (!$('#entry-form').hasClass('hidden')) { //hide entry-form if visible
+      $('#entry-form').toggleClass('hidden');
+    }
+  } else { //show the initial screen
+    if (!(dimension === undefined)) {$('#dimension').val(dimension);}
+    if (!$('#app').hasClass('hidden')) { //hide app if visible
+      $('#app').toggleClass('hidden');
+    }
+    if ($('#entry-form').hasClass('hidden')) { //show entry-form if hidden
       $('#entry-form').toggleClass('hidden');
     }
   }
